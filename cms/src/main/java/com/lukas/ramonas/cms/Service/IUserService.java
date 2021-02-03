c402ecad42c10a4a979980afc82a9f408d5d8bc0
MexMass <ramonas.lukas@gmail.com>
2021-02-03 20:55:06 +0000

    Create Iuserservice
    Signed-off-by: MexMass <ramonas.lukas@gmail.com>

---
+++ cms/src/main/java/com/lukas/ramonas/cms/Service/IUserService.java
@@ -0,0 +1,10 @@
+package com.lukas.ramonas.cms.Service;
+
+import com.lukas.ramonas.cms.DAO.UserDto;
+import com.lukas.ramonas.cms.Exceptions.UserAlreadyExistException;
+import com.lukas.ramonas.cms.Model.User;
+
+public interface IUserService {
+    User registerNewUserAccount(UserDto userDto)
+            throws UserAlreadyExistException;
+}
