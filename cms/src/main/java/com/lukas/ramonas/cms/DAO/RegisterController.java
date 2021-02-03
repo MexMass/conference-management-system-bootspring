971fb3caa92d08823effbe2755642f2e313da019
MexMass <ramonas.lukas@gmail.com>
2021-02-03 15:26:34 +0000

    Created separate controller for register process
    Signed-off-by: MexMass <ramonas.lukas@gmail.com>

---
+++ cms/src/main/java/com/lukas/ramonas/cms/DAO/RegisterController.java
@@ -0,0 +1,29 @@
+package com.lukas.ramonas.cms.DAO;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+
+@Controller // This means that this class is a Controller
+public class RegisterController {
+
+    @GetMapping("/user/registration")
+    public String showRegistrationForm(WebRequest request, Model model) {
+        UserDto userDto = new UserDto();
+        model.addAttribute("user", userDto);
+        return "registration";
+    }
+
+    public ModelAndView registerUserAccount(
+            @ModelAttribute("user") @Valid UserDto userDto,
+            HttpServletRequest request, Errors errors) {
+        return null;
+    }
+}
