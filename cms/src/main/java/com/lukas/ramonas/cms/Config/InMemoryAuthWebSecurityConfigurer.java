1d40b8a1208c40045a285c936e326f186b5124b4
MexMass <ramonas.lukas@gmail.com>
2021-02-03 20:40:09 +0000

    Update RegisterController
    Signed-off-by: MexMass <ramonas.lukas@gmail.com>

--- cms/src/main/java/com/lukas/ramonas/cms/Config/InMemoryAuthWebSecurityConfigurer.java
+++ cms/src/main/java/com/lukas/ramonas/cms/Config/InMemoryAuthWebSecurityConfigurer.java
@@ -1,6 +1,6 @@
 package com.lukas.ramonas.cms.Config;
 
-import com.lukas.ramonas.cms.DAO.MyUserDetailsService;
+import com.lukas.ramonas.cms.Service.MyUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
--- cms/src/main/java/com/lukas/ramonas/cms/DAO/RegisterController.java
+++ cms/src/main/java/com/lukas/ramonas/cms/DAO/RegisterController.java
@@ -1,10 +1,14 @@
 package com.lukas.ramonas.cms.DAO;
 
+import com.lukas.ramonas.cms.Exceptions.UserAlreadyExistException;
+import com.lukas.ramonas.cms.Model.User;
+import com.lukas.ramonas.cms.Service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -14,6 +18,10 @@ import javax.validation.Valid;
 @Controller // This means that this class is a Controller
 public class RegisterController {
 
+    // ++++++++++++++++++ The below ModelandView might be not correct LOOK INTO THIS IN THE FUTURE IF STUFF DOESNT WORK
+    // ==================
+    ModelAndView mav = new ModelAndView("registerUserAccount");
+
     @GetMapping("/user/registration")
     public String showRegistrationForm(WebRequest request, Model model) {
         UserDto userDto = new UserDto();
@@ -21,9 +29,24 @@ public class RegisterController {
         return "registration";
     }
 
-    public ModelAndView registerUserAccount(
-            @ModelAttribute("user") @Valid UserDto userDto,
-            HttpServletRequest request, Errors errors) {
-        return null;
+    @PostMapping("/user/registration")
+    public ModelAndView registerUserAccount
+            (@ModelAttribute("user") @Valid UserDto userDto,
+             HttpServletRequest request, Errors errors) {
+
+        try {
+            /* ++++++++++++++++ WATCH OUT FOR THIS USERSERVICE AS IT MIGHT NOT BE IN THE RIGHT PLACE
+            *  Followed example online on baeldung.com although they did not use UserService UserService = new UserService();
+            * in this example.
+            * */
+            UserService UserService = new UserService();
+            User registered = UserService.registerNewUserAccount(userDto);
+        } catch (UserAlreadyExistException uaeEx) {
+            mav.addObject("message", "An account for that username/email already exists.");
+            return mav;
+        }
+
+        return new ModelAndView("successRegister", "user", userDto);
     }
+
 }
