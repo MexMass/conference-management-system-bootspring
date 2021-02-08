5b29effb0a992c8ae463d4c56a3cd556f2c58644
MexMass <ramonas.lukas@gmail.com>
2021-02-08 20:16:56 +0000

    Update Privilege.java added @Table annotation which fixed the program not recognising Privilege
    Signed-off-by: MexMass <ramonas.lukas@gmail.com>

--- cms/src/main/java/com/lukas/ramonas/cms/Model/Privilege.java
+++ cms/src/main/java/com/lukas/ramonas/cms/Model/Privilege.java
@@ -7,6 +7,7 @@ import java.util.Collection;
  * Defined privilege model
  *******************************************/
 @Entity
+@Table(name = "privilege_table", schema = "public")
 public class Privilege {
 
     @Id
