df4563586263c8788178861d73288a2ad04767a3
MexMass <ramonas.lukas@gmail.com>
2021-02-16 19:29:30 +0000

    Added RoleRepository vital for setting the right manyToMany relationships in the database
    Signed-off-by: MexMass <ramonas.lukas@gmail.com>

---
+++ cms/src/main/java/com/lukas/ramonas/cms/DAO/RoleRepository.java
@@ -0,0 +1,9 @@
+package com.lukas.ramonas.cms.DAO;
+
+import com.lukas.ramonas.cms.Model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+    Role findByName(String name);
+}
