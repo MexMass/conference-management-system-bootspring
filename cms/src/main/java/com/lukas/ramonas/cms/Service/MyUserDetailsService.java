c3468ef4b50c6ac5960a1f28cc1e8b7fab6d206a
MexMass <ramonas.lukas@gmail.com>
2021-04-26 02:16:55 +0100

    update
    Signed-off-by: MexMass <ramonas.lukas@gmail.com>

--- cms/src/main/java/com/lukas/ramonas/cms/Service/MyUserDetailsService.java
+++ cms/src/main/java/com/lukas/ramonas/cms/Service/MyUserDetailsService.java
@@ -5,7 +5,6 @@ import com.lukas.ramonas.cms.Model.Role;
 import com.lukas.ramonas.cms.Model.User;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import org.springframework.security.core.GrantedAuthority;
