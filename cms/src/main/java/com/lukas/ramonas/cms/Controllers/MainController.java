d10b4f5b74cbcd1cb1a8608eb5ba4e31289e9c90
MexMass <ramonas.lukas@gmail.com>
2021-04-27 00:17:03 +0100

    update
    Signed-off-by: MexMass <ramonas.lukas@gmail.com>

--- cms/src/main/java/com/lukas/ramonas/cms/Controllers/MainController.java
+++ cms/src/main/java/com/lukas/ramonas/cms/Controllers/MainController.java
@@ -1,11 +1,6 @@
 package com.lukas.ramonas.cms.Controllers;
 
-import com.lukas.ramonas.cms.DAO.UserRepository;
-import com.lukas.ramonas.cms.Model.User;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
