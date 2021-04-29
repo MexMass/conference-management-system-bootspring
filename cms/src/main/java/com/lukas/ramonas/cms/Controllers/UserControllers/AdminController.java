e3a0566ce973d4acfc4585e41f6fc38675054ec8
MexMass <ramonas.lukas@gmail.com>
2021-04-29 07:13:02 +0100

    update
    Signed-off-by: MexMass <ramonas.lukas@gmail.com>

--- cms/src/main/java/com/lukas/ramonas/cms/Controllers/UserControllers/AdminController.java
+++ cms/src/main/java/com/lukas/ramonas/cms/Controllers/UserControllers/AdminController.java
@@ -1,11 +1,15 @@
 package com.lukas.ramonas.cms.Controllers.UserControllers;
 
+import com.lukas.ramonas.cms.Model.Conference;
 import com.lukas.ramonas.cms.Model.Role;
 import com.lukas.ramonas.cms.Model.User;
+import com.lukas.ramonas.cms.Service.IConferenceService;
 import com.lukas.ramonas.cms.Service.IUserService;
 import com.lukas.ramonas.cms.Service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -22,6 +26,9 @@ public class AdminController {
     @Autowired
     private IUserService userService;
 
+    @Autowired
+    private IConferenceService conferenceService;
+
     @GetMapping(path="/adminIndex")
     public String showAdminIndex(){
         return "adminIndex";
@@ -34,6 +41,14 @@ public class AdminController {
         model.addAttribute("userList", userList);
         return "users";
     }
+
+    @GetMapping(path="/conferences")
+    public String showConferencesPage(Model model){
+        List<Conference> conferenceList = conferenceService.findAll();
+        model.addAttribute("conferenceList", conferenceList);
+        return "configure";
+    }
+
     @GetMapping(value = "/delete/{id}")
     public String deleteUser(@PathVariable(name = "id") Long id, RedirectAttributes redirectAttributes) {
     userService.delete(id);
